1cb94cbcb11efd8eecebc722474d4bc97298831f
PooyaRezaei <pooya.rezaee.official@gmail.com>
2024-11-15 18:32:33 +0330

    add flash message

--- apps/account/views.py
+++ apps/account/views.py
@@ -4,6 +4,7 @@ from django.views import View
 from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.db import IntegrityError
+from django.contrib import messages
 from .mixins import NoLoginMixin
 from .forms import LoginForm, RegisterForm, ProfileForm
 from .models import User
@@ -22,11 +23,12 @@ class RegisterView(NoLoginMixin, View):
             data = form.cleaned_data
             try:
                 user = User.objects.create_user(email=data["email"],password=data["password"])
-                print("Successfly created")
+                messages.success(request,"You have successfully registered.")
                 return redirect(reverse("main:home"))
             except IntegrityError:
-                print("This email has already been registered.")
-        print(form.errors)
+                messages.warning(request,"This email has already been registered.")
+        else:
+            messages.warning(request,"Form not valid.")
         return render(request,"account/register.html",{"form":form})
 
 
@@ -44,20 +46,20 @@ class CustomLoginView(NoLoginMixin, View):
             user = authenticate(request, email=data["email"], password=data["password"])
             if user is not None:
                 login(request,user)
-                print('You Logged')
+                messages.success(request,'You have successfully logged in.')
                 return redirect('main:home')
             else:
-                print('UserName or Password Is Wrong')
-        
-        print(form.errors)
+                messages.warning(request,'UserName or Password Is Wrong.')
+        else:
+            messages.warning(request,"form not valid.")
         return render(request,"account/login.html",{"form":form})
+
 
 class LogoutView(LoginRequiredMixin,View):
     def get(self, request):
         logout(request)
-        print('You Logouted')
+        messages.success(request,"You have successfully registered.")
         return redirect('main:home')
-from django.contrib import messages
 
 
 class UpdateProfileView(LoginRequiredMixin,View):
@@ -70,14 +72,14 @@ class UpdateProfileView(LoginRequiredMixin,View):
 
     def post(self,request):
         if request.POST.get('email') != request.user.email:
-            # messages.error(request, "You don't have access to change email.")
-            print("Email changed")
+            messages.error(request, "You don't have access to change email.")
             return redirect('account:profile')
         
         form = self.form_class(data=request.POST, instance=request.user)
         if form.is_valid():
             form.save()
-            print("Form updated")
+            messages.success(request,"Profile Updated")
             return redirect(reverse("main:home"))
 
+        messages.warning(request,"Form not valid.")
         return render(request,"account/profile.html",{"form":form})
